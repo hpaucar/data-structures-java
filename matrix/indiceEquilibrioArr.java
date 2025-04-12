2aa08ebe38d05db48dd2ff734edfb01cf7bc69f5
Mauricio Cisneros <smlaulet@gmail.com>
2025-04-12 01:50:24 -0500

    Se agregan 3 ejercicios de Arreglos y Matrices

---
+++ matrix/indiceEquilibrioArr.java
@@ -0,0 +1,14 @@
+public class indiceEquilibrioArr {
+    public static int findEquilibriumIndex(int[] arr) {
+
+        // Codigo Aquí
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -7, 1, 5, 2, -4, 3, 0 };
+        int index = findEquilibriumIndex(arr);
+        System.out.println("Equilibrium Index: " + index);
+    }
+}
---
+++ matrix/indiceEquilibrioMatrix.java
@@ -0,0 +1,18 @@
+public class indiceEquilibrioMatrix {
+    public static int findEquilibriumIndex(int[][] arr) {
+
+        // Codigo Aquí
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+                { 2, 3, 5, 1 },
+                { 4, 1, 1, 6 },
+                { 9, 2, 3, 1 }
+        };
+        int index = findEquilibriumIndex(matriz);
+        System.out.println("Equilibrium Index: " + index);
+    }
+}
