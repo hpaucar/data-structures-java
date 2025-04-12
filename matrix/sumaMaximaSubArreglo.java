31fa30077d04aa276bf660e70ff8ed8b8a408279
Mauricio Cisneros <smlaulet@gmail.com>
2025-04-12 13:35:47 -0500

    Solución esperada, fuerza bruta. Kadane sería ideal.

---
+++ matrix/sumaMaximaSubArreglo.java
@@ -0,0 +1,16 @@
+public class sumaMaximaSubArreglo {
+
+    public static int findMaxSubArraySum(int[] arr) {
+
+        // Codigo Aquí
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int maxSum = findMaxSubArraySum(arr);
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+
+}
